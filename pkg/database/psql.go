88ed3fbe9d2a29a0defd3e3ad5d8279e71c5bc29
Andrei Dubov <av.dubov@yandex.ru>
2024-03-03 10:40:48 +0400

    added postgres connection entity

---
+++ pkg/database/psql.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/andredubov/sso/internal/config"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func NewPostgresConnection(cfg config.Config) (*sqlx.DB, error) {
+
+	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s",
+		cfg.Postgres.Host,
+		cfg.Postgres.Port,
+		cfg.Postgres.Username,
+		cfg.Postgres.DatabaseName,
+		cfg.Postgres.SSLMode,
+		cfg.Postgres.Password))
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
