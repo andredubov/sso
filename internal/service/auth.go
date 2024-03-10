29399719a4a69df5bbddb1c712a67f3bd9707f3e
Andrei Dubov <av.dubov@yandex.ru>
2024-03-10 18:07:36 +0400

    added auth service implementation

---
+++ internal/service/auth.go
@@ -0,0 +1,91 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/andredubov/sso/internal/config"
+	"github.com/andredubov/sso/internal/domain/model"
+	"github.com/andredubov/sso/internal/repository"
+	"github.com/andredubov/sso/pkg/auth"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+)
+
+// authService apps service entity
+type authService struct {
+	repository   *repository.Repository
+	tokenManager auth.TokenManager
+	jwtConfig    config.JWTConfig
+}
+
+// NewAuthService create an instance of the authService struct
+func NewAuthService(repo *repository.Repository, manager auth.TokenManager, cfg config.JWTConfig) Auth {
+	return &authService{
+		repository:   repo,
+		tokenManager: manager,
+		jwtConfig:    cfg,
+	}
+}
+
+// SignUp registers a new user
+func (a *authService) SignUp(ctx context.Context, user model.User) (string, error) {
+
+	const op = "authService.SignUp"
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	user.Password = string(passwordHash)
+
+	return a.repository.Users.Add(ctx, user)
+}
+
+// SignIn a new user by its email and password in app by its id
+func (a *authService) SignIn(ctx context.Context, email, password, appID string) (string, error) {
+
+	const op = "authService.SignIn"
+
+	user, err := a.repository.Users.GetByEmail(ctx, email)
+	if err != nil {
+		if errors.Is(err, repository.ErrUserNotFound) {
+			return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+		}
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return "", fmt.Errorf("%s: %w", op, ErrInvalidCredentials)
+	}
+
+	app, err := a.repository.Apps.GetByID(ctx, appID)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	token, err := a.tokenManager.NewJWT(user, app, a.jwtConfig.AccessTokenTTL)
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return token, err
+}
+
+// IsAdmin found out weather the user is an admin or no
+func (a *authService) IsAdmin(ctx context.Context, userID string) (bool, error) {
+
+	const op = "authService.IsAdmin"
+
+	isAdmin, err := a.repository.Users.IsAdmin(ctx, userID)
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return isAdmin, nil
+}
