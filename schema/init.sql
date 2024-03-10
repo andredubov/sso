1c83d1587bb7eb2c39826c5b3c04198db047a2e2
Andrei Dubov <av.dubov@yandex.ru>
2024-03-10 10:12:32 +0400

    create index for email column on to the users database table

--- schema/init.sql
+++ schema/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users 
+CREATE TABLE IF NOT EXISTS users 
 (
     id uuid primary key default gen_random_uuid(),
     email varchar(255) not null unique,
@@ -6,7 +6,9 @@ CREATE TABLE users
     is_admin boolean not null default false
 );
 
-CREATE TABLE apps
+CREATE INDEX IF NOT EXISTS email_idx ON users (email);
+
+CREATE TABLE IF NOT EXISTS apps
 (
     id uuid primary key default gen_random_uuid(),
     name varchar(255) not null unique,
