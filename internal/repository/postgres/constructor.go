b7b5f2483e67e4a825b848b30c845b9a9d832f1a
Andrei Dubov <av.dubov@yandex.ru>
2024-03-16 18:50:03 +0400

    fixed a comment

--- internal/repository/postgres/constructor.go
+++ internal/repository/postgres/constructor.go
@@ -5,7 +5,7 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-// NewRepository create an instance of the repository.Repository struct
+// NewRepository returns a new instance of the repository.Repository struct
 func NewRepository(db *sqlx.DB) *repository.Repository {
 	return &repository.Repository{
 		Users: NewUsersRepository(db),
