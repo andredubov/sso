b9fc150488b3ac5815e328bc4aa049bfb3eee663
Andrei Dubov <av.dubov@yandex.ru>
2024-03-16 16:11:58 +0400

    refactored application creator service

--- internal/service/app.go
+++ internal/service/app.go
@@ -2,25 +2,41 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/andredubov/sso/internal/domain/model"
 	"github.com/andredubov/sso/internal/repository"
 )
 
-type appsCreator struct {
+var (
+	ErrAppExists = errors.New("application already exists")
+)
+
+type appCreator struct {
 	appsRepository repository.Apps
 }
 
 // NewAppCreater returns a new instance app creator service
 func NewAppCreator(appsRepo repository.Apps) AppCreator {
-	return &appsCreator{
+	return &appCreator{
 		appsRepository: appsRepo,
 	}
 }
 
 // Create registers a new application in the system and returns application ID
 // If application with given name already exists return error
-func (a *appsCreator) Create(ctx context.Context, name, secret string) (string, error) {
+func (a *appCreator) Create(ctx context.Context, name, secret string) (string, error) {
 
-	return a.appsRepository.Add(ctx, model.App{Name: name, Secret: secret})
+	const op = "appCreator.Create"
+
+	id, err := a.appsRepository.Add(ctx, model.App{Name: name, Secret: secret})
+	if err != nil {
+		if errors.Is(err, repository.ErrAppExists) {
+			return "", fmt.Errorf("%s: %w", op, ErrAppExists)
+		}
+		return "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
 }
