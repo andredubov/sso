4612444041a8b8d72fcefbfbeadb1996fdc9bc70
Andrei Dubov <av.dubov@yandex.ru>
2024-03-16 12:33:44 +0400

    refactored app grpc server

---
+++ internal/transport/grpc/app/v2/server.go
@@ -0,0 +1,46 @@
+package server
+
+import (
+	"context"
+	"errors"
+
+	ssov2 "github.com/andredubov/protos/v2/gen/go/sso"
+	"github.com/andredubov/sso/internal/repository"
+	"github.com/andredubov/sso/internal/service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type appsGRPCServer struct {
+	ssov2.UnimplementedAppsServer
+	appCreator service.AppCreator
+}
+
+// NewGRPCAppsServer returns a new instance of application grpc server
+func NewGRPCAppsServer(appCreator service.AppCreator) ssov2.AppsServer {
+	return &appsGRPCServer{
+		appCreator: appCreator,
+	}
+}
+
+// Create handles grpc request to register a new application in the system and returns result in grpc response
+func (a *appsGRPCServer) Create(ctx context.Context, request *ssov2.CreateRequest) (*ssov2.CreateResponse, error) {
+
+	if request.AppName == "" {
+		return nil, status.Error(codes.InvalidArgument, "application name is required")
+	}
+
+	if request.AppSecret == "" {
+		return nil, status.Error(codes.InvalidArgument, "application secret is required")
+	}
+
+	id, err := a.appCreator.Create(ctx, request.AppName, request.AppSecret)
+	if err != nil {
+		if errors.Is(err, repository.ErrAppExists) {
+			return nil, status.Error(codes.AlreadyExists, "application already exists")
+		}
+		return nil, status.Error(codes.Internal, "failed to create an application")
+	}
+
+	return &ssov2.CreateResponse{AppId: id}, nil
+}
