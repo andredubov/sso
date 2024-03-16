977d055bb591ab197a339c2420bb321e231e97f0
Andrei Dubov <av.dubov@yandex.ru>
2024-03-16 11:49:45 +0400

    refactored app service

--- internal/service/app.go
+++ internal/service/app.go
@@ -11,13 +11,15 @@ type appsCreator struct {
 	appsRepository repository.Apps
 }
 
-// NewAppCreater
+// NewAppCreater returns a new instance app creator service
 func NewAppCreator(appsRepo repository.Apps) AppCreator {
 	return &appsCreator{
 		appsRepository: appsRepo,
 	}
 }
 
+// Create registers a new application in the system and returns application ID
+// If application with given name already exists return error
 func (a *appsCreator) Create(ctx context.Context, name, secret string) (string, error) {
 
 	return a.appsRepository.Add(ctx, model.App{Name: name, Secret: secret})
